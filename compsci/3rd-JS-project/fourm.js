e3ec04c492b3d83b56d6d54e34f3b51eb045287f
andrew <andrewds@cox.net>
2018-03-29 08:27:17 -0700

    This is the 3rd javascript project that I am working on for school this is the version that is going to be turned in and uploaded to my webserver on 3-29-18

---
+++ compsci/3rd-JS-project/fourm.js
@@ -0,0 +1,18 @@
+function output() {
+    //this grabs the input from the first name field so I cam use it in javascript
+    let firstname = document.getElementById('inputfirstname').value;
+    //this grabs the last name data so it can be manipulated by javascript
+    let lastname = document.getElementById('inputlastname').value;
+    //this grabs the first number to be added later
+    let inputnumber = document.getElementById('inputnumber').value;
+    //this grabs the second number to be manipulated later
+    let inputnumber2 = document.getElementById('inputnumber2').value;
+    //this adds the two numbers together
+    let numtotal = inputnumber + inputnumber2;
+    //this finds the square root of the previous number
+    let sqrtnumber = Math.sqrt(numtotal);
+
+
+    //this prints the output to the screen of all the inputs and some static content
+    document.getElementById('result').innerHTML='Your name is ' + firstname + ' ' + lastname + ' those two numbers added together = ' + numtotal + ' the square root of those numbers is ' + sqrtnumber;
+}
